a69710a0d6d5b4edddb0e0c37fb9d80fb3767b19
liuzhiyi1992 <55137840@qq.com>
2016-05-14 22:25:09 +0800

    create repo

---
+++ SpeedFreezingVideo/CaptureVideoViewController.m
@@ -0,0 +1,37 @@
+//
+//  CaptureVideoViewController.m
+//  SpeedFreezingVideo
+//
+//  Created by lzy on 16/5/14.
+//  Copyright © 2016年 lzy. All rights reserved.
+//
+
+#import "CaptureVideoViewController.h"
+
+@interface CaptureVideoViewController ()
+
+@end
+
+@implementation CaptureVideoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
